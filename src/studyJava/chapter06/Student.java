02d68041de011d3685e9bdce7b39752a559425e0
123748712 <kemco01@naver.com>
2021-12-16 17:47:53 +0900

    21.12.16 객체 지향 프로그래밍

---
+++ src/studyJava/chapter06/Student.java
@@ -0,0 +1,6 @@
+package studyJava.chapter06;
+
+public class Student {
+	// 객체와 동일한 이름으로 class 생성
+
+}
---
+++ src/studyJava/chapter06/StudentExample.java
@@ -0,0 +1,14 @@
+package studyJava.chapter06;
+
+public class StudentExample {
+public static void main(String[] args) {
+	/* 
+	 *  1. 객체
+	 *  객체는 속성과 동작으로 구성되어 있으며, 속성은 필드, 동작은 메서드 라고 칭한다.
+	 */
+	Student s1 = new Student();
+	Student s2 = new Student();  // 객체 (인스턴스) 생성
+	
+	
+}
+}
