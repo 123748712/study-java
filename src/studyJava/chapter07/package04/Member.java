fc7e5cb3b82acf89a3c3bd22a910570747a8ac11
123748712 <kemco01@naver.com>
2021-12-22 14:48:54 +0900

    21.12.22 final 클래스

---
+++ src/studyJava/chapter07/package04/Member.java
@@ -0,0 +1,12 @@
+package studyJava.chapter07.package04;
+
+public final class Member { // final 클래스 선언
+
+	/*
+	 *  1. final 클래스
+	 *  
+	 *  클래스를 선언할떄 final 키워드를 class 앞에 붙이면 이 클래스는 최종적인 클래스이므로
+	 *  상속할 수 없는 클래스가 된다.
+	 *  즉, final 클래스는 부모 클래스가 될 수 없어 자식 클래스를 만들 수 없다는 것.
+	 */
+}
