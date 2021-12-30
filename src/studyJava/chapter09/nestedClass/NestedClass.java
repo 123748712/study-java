634363b80e2588270295a8a51098fb38b6d14ef0
123748712 <kemco01@naver.com>
2021-12-30 17:39:59 +0900

    21.12.30 중첩클래스 이론

---
+++ src/studyJava/chapter09/nestedClass/NestedClass.java
@@ -0,0 +1,22 @@
+package studyJava.chapter09.nestedClass;
+
+public class NestedClass {
+
+	/*
+	 * 1. 중첩 클래스
+	 * 클래스 내부에 선언한 클래스
+	 * 클래스의 멤버로 선언한 중첩 클래스 -> 멤버클래스
+	 * 생성자 또는 메소드 내부에 선언된 중첩 클래스 -> 로컬 클래스
+	 */
+	class NestedClassName{ // 중첩 클래스
+		
+	}
+	
+	/*
+	 * 2. 중첩 인터페이스
+	 * 클래스 내부에 선언한 인터페이스
+	 */
+	interface NestedInterfaceName{ // 중첩 인터페이스
+		
+	}
+}
