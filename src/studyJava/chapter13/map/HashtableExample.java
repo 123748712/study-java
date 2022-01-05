df90863508793509d4db23050c747ee849a072f3
123748712 <kemco01@naver.com>
2022-01-05 10:17:58 +0900

    22.01.05 Hashtable

---
+++ src/studyJava/chapter13/map/HashtableExample.java
@@ -0,0 +1,13 @@
+package studyJava.chapter13.map;
+
+public class HashtableExample {
+	public static void main(String[] args) {
+		/*
+		 * 	1. Hashtable
+		 * 	HashMap 과 동일한 내부 구조를 갖고 있다.
+		 * 	동일하게 hashCode() 와 equals() 를 재정의 해야한다.
+		 * 	
+		 * 	HashMap을 가장 많이 쓴다.
+		 */
+	}
+}
