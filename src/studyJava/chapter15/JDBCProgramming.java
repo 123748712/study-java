be799d75b3b85c0240bc6ad529544ef9e58d4c4b
123748712 <kemco01@naver.com>
2022-01-05 11:16:52 +0900

    22.01.05 JDBC 간단설명

---
+++ src/studyJava/chapter15/JDBCProgramming.java
@@ -0,0 +1,19 @@
+package studyJava.chapter15;
+
+public class JDBCProgramming {
+	public static void main(String[] args) {
+		// JDBC : 자바와 데이터베이스와의 만남
+		// Java DataBase Connection
+		
+		/*
+		 * 	0. JDBC Driver 필요
+		 * 	1. 접속(접속 정보 : 접속할 DB, 아이디, 비밀번호)
+		 * 	2. 쿼리를 작성할 워크시트를 만든다.
+		 * 	3. 쿼리 작성
+		 * 	4. 작성한 쿼리를 실행
+		 * 	5. 결과를 화면에 표시
+		 * 	6. 종료
+		 */
+		
+	}
+}
